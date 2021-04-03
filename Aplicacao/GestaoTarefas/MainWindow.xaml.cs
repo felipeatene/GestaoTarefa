9d9fd9ae7f2fe8d205c7a44b1c4c350a828af242
Felipe Atene Silva <felipe.atene@sinqia.com.br>
2021-04-03 03:30:26 -0300

    Inclusão Projeto Gestão Tarefas

---
+++ Aplicacao/GestaoTarefas/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using GestaoTarefas.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GestaoTarefas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnNovoUsuario_Click(object sender, RoutedEventArgs e)
+        {
+            frmCadastro telaCadastro = new frmCadastro();
+            this.Visibility = System.Windows.Visibility.Collapsed;
+            telaCadastro.ShowDialog();
+            this.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void BtnEntrar_Click(object sender, RoutedEventArgs e)
+        {
+
+            frmDashboard telaDashboard = new frmDashboard();
+            this.Visibility = System.Windows.Visibility.Collapsed;
+            telaDashboard.ShowDialog();
+            this.Visibility = System.Windows.Visibility.Visible;
+        }
+    }
+}
---
+++ Aplicacao/GestaoTarefas/View/frmDashboard.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GestaoTarefas.View
+{
+    /// <summary>
+    /// Interaction logic for frmDashboard.xaml
+    /// </summary>
+    public partial class frmDashboard : Window
+    {
+        public frmDashboard()
+        {
+            InitializeComponent();
+        }
+
+        public List<string> ListaAFazer { get; set; }
+
+        private void BtnAddTarefa_Click(object sender, RoutedEventArgs e)
+        {
+            lstAFazer.Items.Add(new { TESTE = txtDescricao.Text });
+        }
+
+        private void TextBox_DragEnter(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
